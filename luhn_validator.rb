7a737ee517e84cbb04d24c2d0b95dbd9b8cb6667
bhimasta <agoengbhimasta@gmail.com>
2018-03-13 21:16:01 +0800

    we now able to check wether the credit card number is valid or not using luhn algorithm

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -4,8 +4,15 @@ module LuhnValidator
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
   def validate_checksum
-    nums_a = number.to_s.chars.map(&:to_i)
-
-    # TODO: use the integers in nums_a to validate its last check digit
+    sum = 0
+    reverse_a = number.to_s.chars.map(&:to_i).reverse!
+    reverse_a.each_with_index do |digit, index|
+      if index.odd?
+        digit *= 2
+        digit -= 9 if digit > 9
+      end
+      sum += digit
+    end
+    sum.modulo(10).zero?
   end
 end
