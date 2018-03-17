cc5997d7688c3ba42d78121242230ba08e801b80
bhimasta <agoengbhimasta@gmail.com>
2018-03-17 18:19:21 +0800

    adding double transposition test, restructure test, rubocop passed

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -1,41 +1,32 @@
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
+require_relative '../double_trans_cipher'
 require 'minitest/autorun'
+
+ciphers = [['Caesar', SubstitutionCipher::Caesar],
+           ['Permutation', SubstitutionCipher::Permutation],
+           ['Double Transposition', DoubleTranspositionCipher]]
 
 describe 'Test card info encryption' do
   before do
-    @cc = CreditCard.new('4916603231464963', 'Mar-30-2020', 'Soumya Ray', 'Visa')
+    @cc = CreditCard.new('4916603231464963',
+                         'Mar-30-2020', 'Soumya Ray', 'Visa')
     @key = 3
   end
 
-  describe 'Using Caesar cipher' do
-    it 'should encrypt card information' do
-      enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
-      enc.wont_equal @cc.to_s
-      enc.wont_be_nil
-    end
+  ciphers.each do |chiper_name, cipher_module|
+    describe "Using #{chiper_name} Cipher" do
+      it 'should encrypt card information' do
+        enc = cipher_module.encrypt(@cc, @key)
+        enc.wont_equal @cc.to_s
+        enc.wont_be_nil
+      end
 
-    it 'should decrypt text' do
-      enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
-      dec = SubstitutionCipher::Caesar.decrypt(enc, @key)
-      dec.must_equal @cc.to_s
-    end
-  end
-
-  describe 'Using Permutation cipher' do
-    it 'should encrypt card information' do
-      enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
-      enc.wont_equal @cc.to_s
-      enc.wont_be_nil
-    end
-
-    it 'should decrypt text' do
-      enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
-      dec = SubstitutionCipher::Permutation.decrypt(enc, @key)
-      dec.must_equal @cc.to_s
+      it 'should decrypt text' do
+        enc = cipher_module.encrypt(@cc, @key)
+        dec = cipher_module.decrypt(enc, @key)
+        dec.must_equal @cc.to_s
+      end
     end
   end
-
-  # TODO: Add tests for double transposition and modern symmetric key ciphers
-  #       Can you DRY out the tests using metaprogramming? (see lecture slide)
 end
