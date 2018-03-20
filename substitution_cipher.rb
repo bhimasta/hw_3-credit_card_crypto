22c8783586977485bbd548e9cd9ef9cb47df3862
jaya <ztriwijaya@gmail.com>
2018-03-20 23:21:47 +0800

    pass spec and rubocop

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -1,4 +1,6 @@
+# This module contains Caesar and Permutation encryption
 module SubstitutionCipher
+  # This module contains Caesar encryption
   module Caesar
     # Encrypts document using key
     # Arguments:
@@ -33,14 +35,23 @@ module SubstitutionCipher
     end
   end
 
+  # This module contains Permutation encryption
   module Permutation
     # Encrypts document using key
     # Arguments:
     #   document: String
     #   key: Fixnum (integer)
     # Returns: String
+
+    def self.call_hash(key)
+      ascii_table = (0..127).to_a.map(&:chr)
+      Hash[ascii_table.zip ascii_table.shuffle(random: Random.new(key))]
+    end
+
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      p_hash = call_hash(key)
+      document.to_s.chars.map { |n| p_hash[n] }.join
     end
 
     # Decrypts String document using integer key
@@ -50,6 +61,8 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      p_hash = call_hash(key)
+      document.to_s.chars.map { |n| p_hash.key(n) }.join
     end
   end
 end
